3ae8c1e5335f5f113113573421202a89a0e5aa4b
devZiyad <164162565+devZiyad@users.noreply.github.com>
2025-05-06 00:33:37 +0300

    installed cord.nvim

--- init.lua
+++ init.lua
@@ -899,7 +899,11 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  { -- Discord Rich Presence for Neovim
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    -- opts = {}
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
